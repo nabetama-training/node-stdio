3155ac3a301110ffd447136b466b3c43eec543a5
nabetama <mao.nabeta@gmail.com>
2018-11-18 00:24:25 +0900

    stream event

---
+++ src/tutorial/events.js
@@ -0,0 +1,23 @@
+import readline from "readline";
+import fs from "fs";
+
+const rs = fs.createReadStream(`${__dirname}/test.txt`, "utf-8");
+const rl = readline.createInterface(rs, {});
+
+// when find a \n, will fire 'line' EventEmitter
+rl.on("line", line => {
+  console.log("Read: ", line);
+  rl.close();
+});
+
+// pipe() connects readable and writable streams
+rs.pipe(process.stdout);
+
+// use EventEmitter
+rl.once("start", msg => {
+  console.log(msg);
+});
+rl.prependListener("start", () => {
+  console.log("Ready...");
+});
+rl.emit("start", "Start!\n================");
